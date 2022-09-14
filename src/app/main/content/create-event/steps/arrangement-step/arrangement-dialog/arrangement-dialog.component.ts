51cfae93d340765669c39f38da84e883667c5aed
Dhruvin <dhruvin.scalelot@gmail.com>
2022-09-15 01:23:27 +0530

    Implement form in arrangement dialog and make dynamic fields(Half commit)

--- src/app/main/content/create-event/steps/arrangement-step/arrangement-dialog/arrangement-dialog.component.ts
+++ src/app/main/content/create-event/steps/arrangement-step/arrangement-dialog/arrangement-dialog.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { CONSTANTS } from 'src/app/main/common/constants';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {CONSTANTS} from 'src/app/main/common/constants';
+import {FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-arrangement-dialog',
@@ -11,14 +12,34 @@ export class ArrangementDialogComponent implements OnInit {
   @Output() isAddEventChange = new EventEmitter<boolean>();
   constants: any = CONSTANTS;
   preparedSeatingItems: any = [];
+  seatingForm: any;
   isArrangement: boolean = false;
-
   selectedTab = 0;
 
-  constructor() { }
+  constructor(
+    private _formBuilder: FormBuilder
+  ) {
+  }
 
   ngOnInit(): void {
     this.preparedSeatingItems = CONSTANTS.seatingItems;
+    this.seatingForm = this._formBuilder.group({
+      seating_item: [''],
+      arrangements: this._formBuilder.array([
+        this._formBuilder.group({
+          number_of_seating_item: ['', [Validators.required]],
+          vertical_location: [0, [Validators.required]],
+          horizontal_location: [0, [Validators.required]],
+          per_table_person: ['', [Validators.required]],
+          total_person: ['', [Validators.required]],
+          per_table_price: ['', [Validators.required]],
+          per_person_price: ['', [Validators.required]],
+          total_amount: ['', [Validators.required]],
+          description: ['', [Validators.required]],
+          booking_acceptance: [false],
+        })
+      ])
+    });
   }
 
   closePopup(): void {
