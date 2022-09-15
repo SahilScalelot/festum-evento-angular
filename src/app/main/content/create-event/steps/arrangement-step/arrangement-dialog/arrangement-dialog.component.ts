f3769a193d029bb68218c611dc5500d06113d062
Dhruvin <dhruvin.scalelot@gmail.com>
2022-09-16 00:14:51 +0530

    Implement form in arrangement dialog and make dynamic fields(Half commit)

--- src/app/main/content/create-event/steps/arrangement-step/arrangement-dialog/arrangement-dialog.component.ts
+++ src/app/main/content/create-event/steps/arrangement-step/arrangement-dialog/arrangement-dialog.component.ts
@@ -1,6 +1,6 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {CONSTANTS} from 'src/app/main/common/constants';
-import {FormBuilder, Validators} from "@angular/forms";
+import {FormArray, FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-arrangement-dialog',
@@ -13,7 +13,6 @@ export class ArrangementDialogComponent implements OnInit {
   constants: any = CONSTANTS;
   preparedSeatingItems: any = [];
   seatingForm: any;
-  isArrangement: boolean = false;
   selectedTab = 0;
 
   constructor(
@@ -40,6 +39,48 @@ export class ArrangementDialogComponent implements OnInit {
         })
       ])
     });
+  }
+
+  get arrangements() {
+    return this.seatingForm.get('arrangements') as FormArray;
+  }
+
+  addArrangements(tempArrangementObj: any = {}): void {
+    if (Object.keys(tempArrangementObj).length === 0) {
+      tempArrangementObj = {
+        number_of_seating_item: '',
+        vertical_location: 0,
+        horizontal_location: 0,
+        per_table_person: '',
+        total_person: '',
+        per_table_price: '',
+        per_person_price: '',
+        total_amount: '',
+        description: '',
+        booking_acceptance: false,
+      };
+    }
+
+    const arrangementsObj = this._formBuilder.group({
+      number_of_seating_item: [tempArrangementObj.number_of_seating_item],
+      vertical_location: [tempArrangementObj.vertical_location],
+      horizontal_location: [tempArrangementObj.horizontal_location],
+      per_table_person: [tempArrangementObj.per_table_person],
+      total_person: [tempArrangementObj.total_person],
+      per_table_price: [tempArrangementObj.per_table_price],
+      per_person_price: [tempArrangementObj.per_person_price],
+      total_amount: [tempArrangementObj.total_amount],
+      description: [tempArrangementObj.description],
+      booking_acceptance: [tempArrangementObj.booking_acceptance],
+    });
+    this.arrangements.push(arrangementsObj);
+  }
+
+  removeArrangement(index: any): void {
+    if (this.arrangements.get(index.toString())) {
+      this.arrangements.removeAt(index.toString());
+      this.arrangements.updateValueAndValidity();
+    }
   }
 
   closePopup(): void {
