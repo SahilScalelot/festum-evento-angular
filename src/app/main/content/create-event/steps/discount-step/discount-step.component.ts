3d78d7bcb4e2f62283106938ceaef8cb29341a9a
Jatin Dhameliya <jatindhameliya1@gmail.com>
2022-11-29 13:20:05 +0530

    Implement new api and change preapre discount functionality

--- src/app/main/content/create-event/steps/discount-step/discount-step.component.ts
+++ src/app/main/content/create-event/steps/discount-step/discount-step.component.ts
@@ -1,12 +1,10 @@
-import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { SnotifyService } from 'ng-snotify';
 import { ModalService } from 'src/app/main/_modal';
 import { CreateEventService } from '../../create-event.service';
 import { GlobalFunctions } from "../../../../common/global-functions";
-import * as _ from 'lodash';
 import { Router } from '@angular/router';
-import { GlobalService } from 'src/app/services/global.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-discount-step',
@@ -16,23 +14,19 @@ import { GlobalService } from 'src/app/services/global.service';
 export class DiscountStepComponent implements OnInit {
   isLoading: boolean = false;
   seatingItems: any = [];
-  getDiscounts: any = [];
+  allDiscounts: any = [];
+  discountList: any = [];
   discountForm: any;
   tmpDiscountObj: any = {};
-  selectedDiscountIds: any = [];
+  selectedDiscountTypes: any = [];
   eventId: any = '';
-
-  @Input() eventObj: any = {};
-  @Output() newEventObj: EventEmitter<any> = new EventEmitter();
 
   constructor(
     private _globalFunctions: GlobalFunctions,
     private _modalService: ModalService,
     private _formBuilder: FormBuilder,
     private _createEventService: CreateEventService,
-    private _sNotify: SnotifyService,
     private _router: Router,
-    private _globalService: GlobalService,
   ) {
   }
 
@@ -43,27 +37,16 @@ export class DiscountStepComponent implements OnInit {
     this.eventId = localStorage.getItem('eId');
     this.tmpDiscountObj = {};
     this.getAllDiscounts();
-    // this.getSeatingItems();
-    this.discountForm = this._formBuilder.group({
-      discount_type: [null, [Validators.required]],
-      equipment_id: [null],
-      discount: [null, [Validators.required]],
-      event_id: [this.eventId || '', [Validators.required]],
-    });
-    this.selectedDiscountIds = this.eventObj.discounts;
+    this.getSeatingItems();
+    this._prepareDiscountForm();
   }
 
   getAllDiscounts(): void {
     this.isLoading = true;
     this._createEventService.getDiscounts().subscribe((result: any) => {
       if (result && result.IsSuccess) {
-        this.getDiscounts = result.Data || [];
-        // _.each(this.getDiscounts, (discount: any) => {
-        //   discount.name = discount.discount_type.replace(/_/g, ' ');
-        //   if (discount.discount_type == 'discount_on_equipment_or_item') {
-        //     discount.equipment_id = _.map(discount.equipment_id, 'equipment_id');
-        //   }
-        // });
+        this.allDiscounts = result.Data || [];
+        this.getDiscountByEventId();
       } else {
         this._globalFunctions.successErrorHandling(result, this, true);
       }
@@ -74,58 +57,43 @@ export class DiscountStepComponent implements OnInit {
     });
   }
 
-  // getSeatingItems(): void {
-  //   this.isLoading = true;
-  //   this._createEventService.getSeatingItems().subscribe((result: any) => {
-  //     if (result && result.IsSuccess) {
-  //       this.seatingItems = result.data || [];
-  //     } else {
-  //       this._globalFunctions.successErrorHandling(result, this, true);
-  //     }
-  //     this.isLoading = false;
-  //   }, (error: any) => {
-  //     this.isLoading = false;
-  //     this._globalFunctions.errorHanding(error, this, true);
-  //   });
-  // }
+  getDiscountByEventId(): void {
+    this.isLoading = true;
+    this._createEventService.getDiscountByEventId(this.eventId).subscribe((result: any) => {
+      if (result && result.IsSuccess) {
+        const selectedDiscounts: any = result.Data.discounts || [];
+        this.selectedDiscountTypes = _.map(selectedDiscounts, 'discounttype');
+        this.discountList = _.values(_.merge(_.keyBy(this.allDiscounts, 'discounttype'), _.keyBy(selectedDiscounts, 'discounttype')));
+      } else {
+        this._globalFunctions.successErrorHandling(result, this, true);
+      }
+      this.isLoading = false;
+    }, (error: any) => {
+      this.isLoading = false;
+      this._globalFunctions.errorHanding(error, this, true);
+    });
+  }
+
+  getSeatingItems(): void {
+    this.isLoading = true;
+    this._createEventService.getSeatingItems().subscribe((result: any) => {
+      if (result && result.IsSuccess) {
+        this.seatingItems = result.Data || [];
+      } else {
+        this._globalFunctions.successErrorHandling(result, this, true);
+      }
+      this.isLoading = false;
+    }, (error: any) => {
+      this.isLoading = false;
+      this._globalFunctions.errorHanding(error, this, true);
+    });
+  }
 
   popupOpen(popId: string, discountObj: any = {}, index: number): void {
     this.tmpDiscountObj = this._globalFunctions.copyObject(discountObj);
     this.tmpDiscountObj.discountIndex = index;
-
-    this.discountForm.patchValue({
-      discount_type: discountObj.discount_type,
-      equipment_id: discountObj.equipment_id,
-      discount: (discountObj.discount.includes('%')) ? discountObj.discount.replace('%', '') : discountObj.discount,
-      event_id: this.eventId
-    });
+    this._prepareDiscountForm(this.tmpDiscountObj);
     this._modalService.open(popId);
-  }
-
-  updateDiscount(): any {
-    this.isLoading = true;
-    const discountObj: any = this.discountForm.value;
-    discountObj.discount = discountObj.discount.toString() + '%';
-
-    // this._createEventService.updateDiscount(discountObj.event_id, this.tmpDiscountObj.id, discountObj).subscribe((result: any) => {
-    //   if (result && result.isSuccess) {
-    //     const discounts = this._globalFunctions.copyObject(this.discounts);
-    //     discounts[this.tmpDiscountObj.discountIndex] = this.tmpDiscountObj;
-    //     discounts[this.tmpDiscountObj.discountIndex].equipment_id = discountObj.equipment_id;
-    //     discounts[this.tmpDiscountObj.discountIndex].discount = discountObj.discount;
-    //     this.discounts = this._globalFunctions.copyObject(discounts);
-
-    //     this._sNotify.success(result.message, 'Success');
-    //     this._modalService.close("discountDialog");
-    //     this.tmpDiscountObj = {};
-    //     this.isLoading = false;
-    //   } else {
-    //     this._globalFunctions.successErrorHandling(result, this, true);
-    //   }
-    // }, (error: any) => {
-    //   this.isLoading = false;
-    //   this._globalFunctions.errorHanding(error, this, true);
-    // });
   }
 
   closePop(): any {
@@ -133,10 +101,60 @@ export class DiscountStepComponent implements OnInit {
     this._modalService.close('discountDialog');
   }
 
+  updateDiscount(): any {
+    const discountObj: any = this.discountForm.value;
+    discountObj.discount = discountObj.discount.toString() + '%';
+
+    const discountList = this._globalFunctions.copyObject(this.discountList);
+    discountList[this.tmpDiscountObj.discountIndex].discount = discountObj.discount || this.tmpDiscountObj.discount;
+    discountList[this.tmpDiscountObj.discountIndex].items = discountObj?.items || [];
+    discountList[this.tmpDiscountObj.discountIndex].isUpdated = true;
+    this.discountList = this._globalFunctions.copyObject(discountList);
+    this._modalService.close('discountDialog');
+    this.tmpDiscountObj = {};
+  }
+
   next(): any {
-    this.eventObj.discounts = this.selectedDiscountIds
-    this.newEventObj.emit(this.eventObj);
-    // this._globalService.addEditEvent$.next(this.selectedDiscountIds);
-    this._router.navigate(['/events/create/company-details']);
+    if (this.selectedDiscountTypes && this.selectedDiscountTypes.length) {
+      this.isLoading = true;
+      const preparedDiscountObj: any = this.prepareDiscountObj(this.selectedDiscountTypes);
+      this._createEventService.createDiscount(preparedDiscountObj).subscribe((result: any) => {
+        if (result && result.IsSuccess) {
+          this._router.navigate(['/events/create/company-details']);
+        } else {
+          this._globalFunctions.successErrorHandling(result, this, true);
+        }
+        this.isLoading = false;
+      }, (error: any) => {
+        this._globalFunctions.errorHanding(error, this, true);
+        this.isLoading = false;
+      });
+    } else {
+      this._router.navigate(['/events/create/company-details']);
+    }
+  }
+
+  prepareDiscountObj(selectedDiscountTypes: any = []): void {
+    const preapredDiscountObj: any = {};
+    preapredDiscountObj.eventid = this.eventId;
+    preapredDiscountObj.discounts = _.map(selectedDiscountTypes, (selectedDiscountType: any) => {
+      const selectedDiscountObj: any = _.find(this.discountList, ['discounttype', selectedDiscountType]);
+      return {
+        discountname: selectedDiscountObj.discountname,
+        discounttype: selectedDiscountObj.discounttype,
+        description: selectedDiscountObj.description,
+        discount: selectedDiscountObj.discount,
+        tandc: selectedDiscountObj.tandc,
+        items: (selectedDiscountObj.isUpdated) ? (selectedDiscountObj?.items || []) : (_.map(selectedDiscountObj?.items || [], '_id') || [])
+      };
+    });
+    return preapredDiscountObj;
+  }
+
+  private _prepareDiscountForm(discountObj: any = {}) {
+    this.discountForm = this._formBuilder.group({
+      discount: [(discountObj.discount && discountObj.discount.includes('%')) ? discountObj.discount.replace('%', '') : discountObj.discount || '', [Validators.required]],
+      items: [(discountObj.isUpdated) ? (discountObj?.items || []) : _.map(discountObj?.items || [], '_id') || []],
+    });
   }
 }
