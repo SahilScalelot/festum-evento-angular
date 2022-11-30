e621a2c7849f90b911197cc9112adef4cea2461f
SahilScalelot <devsahil.scalelot@gmail.com>
2022-11-30 17:51:38 +0530

    Event Flow Done

--- src/app/main/content/event/event-overview/event-overview.component.ts
+++ src/app/main/content/event/event-overview/event-overview.component.ts
@@ -47,9 +47,6 @@ export class EventOverviewComponent implements OnInit {
     const eventId = this._activatedRoute.snapshot.paramMap.get('id');
     this._eventService.getSingleEvents(eventId).subscribe((result: any) => {
       this.events = result.Data;
-      console.log(this.events);
-      // console.log(this.events[0].t_and_c);
-      // this.t_and_c = this.events[0].t_and_c
       setTimeout(() => {
         this._globalFunctions.loadAccordion();
         // this._globalFunctions.loadTabsJs();
