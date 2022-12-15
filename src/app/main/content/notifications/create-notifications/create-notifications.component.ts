239c78fae119693cb5f060547e29af409e55d2df
SahilScalelot <devsahil.scalelot@gmail.com>
2022-12-15 20:53:32 +0530

    create New Notification

--- src/app/main/content/notifications/create-notifications/create-notifications.component.ts
+++ src/app/main/content/notifications/create-notifications/create-notifications.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { SnotifyService } from 'ng-snotify';
 
 @Component({
   selector: 'app-create-notifications',
@@ -7,7 +10,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateNotificationsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _router: Router,
+    private _sNotify: SnotifyService,
+  ) { }
 
   ngOnInit(): void {
   }
