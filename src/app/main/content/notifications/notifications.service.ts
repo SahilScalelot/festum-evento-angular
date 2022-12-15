c42b3c502cc57187459b0b520acb3957cb04995d
Dhruvin <dhruvin.scalelot@gmail.com>
2022-12-15 22:28:37 +0530

    Implement notifications services(api)

--- src/app/main/content/notifications/notifications.service.ts
+++ src/app/main/content/notifications/notifications.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {GlobalFunctions} from "../../common/global-functions";
+import {environment} from "../../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationsService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private _globalFunctions: GlobalFunctions) { }
+
+  // Notification List Api
+  onlineOffersList(pageLimit: any): any {
+    return this.http.post(environment.appURL + 'organizer/onlineoffer', pageLimit, this._globalFunctions.getAuthorizationHeader());
+  }
+
+  // Create Notification Api
+  createOnlineOffer(offerObj: any): any {
+    return this.http.post(environment.appURL + 'organizer/onlineoffer/save', offerObj, this._globalFunctions.getAuthorizationHeader());
+  }
+
+  // Get Notification By Id Api
+  getOnlineOfferById(notificationId: any): any {
+    return this.http.post(environment.appURL + 'organizer/notification/getone', {notificationid: notificationId}, this._globalFunctions.getAuthorizationHeader());
+  }
+
+  // Photo Upload Api
+  uploadBanner(photoFormData: any): any {
+    return this.http.post(environment.appURL + 'organizer/notification/banner', photoFormData, this._globalFunctions.getFileAuthorizationHeader());
+  }
 }
