be87d700de5519fe094b1bedf46748a48b2092a8
SahilScalelot <devsahil.scalelot@gmail.com>
2022-12-21 19:31:23 +0530

    Live Overview

--- src/app/main/content/live-stream/stream-overview/stream-overview.component.ts
+++ src/app/main/content/live-stream/stream-overview/stream-overview.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CONSTANTS } from 'src/app/main/common/constants';
+import { GlobalFunctions } from 'src/app/main/common/global-functions';
+import { LiveStreamService } from '../live-stream.service';
 
 @Component({
   selector: 'app-stream-overview',
@@ -6,10 +10,76 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stream-overview.component.scss']
 })
 export class StreamOverviewComponent implements OnInit {
+  liveStreamObj: any = [];
+  constants: any = CONSTANTS;
+  isLoading: boolean = false;
+  isOpenPopup: boolean = false;
+  isImage: boolean = false;
+  companyIAndV: boolean = false;
+  imagesOrVideosArr: Array<any> = [];
 
-  constructor() { }
+  overview: boolean = true;
+  attendee: boolean = false;
+  reviews: boolean = false;
 
-  ngOnInit(): void {
+  zoom: number = CONSTANTS.defaultMapZoom;
+  // initial center position for the map
+  lat: number = 0;
+  lng: number = 0;
+
+  constructor(
+    public _globalFunctions: GlobalFunctions,
+    private _activatedRoute: ActivatedRoute,
+    private _router: Router,
+    private _liveStreamService: LiveStreamService,
+  ) {
   }
 
+  ngOnInit(): void {
+    this.getEvent();    
+  }
+
+  getEvent(): void {
+    this.isLoading = true;
+    const liveStreamId = this._activatedRoute.snapshot.paramMap.get('id');
+    this._liveStreamService.getLiveStreamById(liveStreamId).subscribe((result: any) => {
+      this.liveStreamObj = result.Data;
+      setTimeout(() => {
+        this._globalFunctions.loadAccordion();
+        // this._globalFunctions.loadTabsJs();
+      }, 0);
+      this.isLoading = false;
+    }, (error: any) => {
+      this._globalFunctions.errorHanding(error, this, true);
+      this.isLoading = false;
+    });
+  }
+
+  onTabChange(tabVarName: any): void {
+    this.overview = this.attendee = this.reviews = false;
+    if (tabVarName == 'overview') {
+      this.overview = true;
+    } else if (tabVarName == 'attendee') {
+      this.attendee = true;
+    } else if (tabVarName == 'reviews') {
+      this.reviews = true;
+    }
+  }
+
+  openImageAndVideoDialog(imagesOrVideosArr: Array<any>, isImage: boolean, companyIAndV: boolean): void {
+    this.imagesOrVideosArr = imagesOrVideosArr;
+    this.isImage = isImage;
+    this.companyIAndV = companyIAndV;
+    this.isOpenPopup = true;
+  }
+
+  closePop(flag: boolean): void {
+    this.isOpenPopup = flag;
+  }
+
+  editEvent(event: any, liveStreamId: any): void {
+    event.stopPropagation();
+    localStorage.setItem('lsId', liveStreamId);
+    this._router.navigate(['/live-stream/create/stream']);
+  }
 }
