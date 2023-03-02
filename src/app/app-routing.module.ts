e27cc9419bc01deb48695d41e4468b4d6658a1e3
SahilScalelot <devsahil.scalelot@gmail.com>
2023-03-02 18:39:54 +0530

    Use #

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -132,7 +132,7 @@ const routes: Routes = [
 ];
 // , { useHash: true }
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: false})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
