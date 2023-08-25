23b839c7159c0f1b75ffafe93be095875a2cf2df
lalji <lalji.scalelot@gmail.com>
2023-08-25 10:23:16 +0530

    list all camera & microphone for web broadcast streaming

--- src/app/main/content/live-stream/broadcast-stream/broadcast-stream.component.ts
+++ src/app/main/content/live-stream/broadcast-stream/broadcast-stream.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, Inject, EventEmitter, Input, OnInit, Output, ViewChild, ElementRef } from '@angular/core';
 import { ActivatedRoute, Router, NavigationStart, Event as NavigationEvent } from '@angular/router';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { DOCUMENT } from '@angular/common';
@@ -6,6 +6,8 @@ import { SnotifyService } from 'ng-snotify';
 import { CONSTANTS } from 'src/app/main/common/constants';
 import { GlobalFunctions } from 'src/app/main/common/global-functions';
 import { LiveStreamService } from '../live-stream.service';
+declare const navigator: any;  // This is the declaration you add
+
 
 @Component({
   selector: 'app-broadcast-stream',
@@ -13,14 +15,15 @@ import { LiveStreamService } from '../live-stream.service';
   styleUrls: ['./broadcast-stream.component.scss']
 })
 export class BroadcastStreamComponent implements OnInit {
-  //@ViewChild('newBroadCastNgForm') newBroadCastNgForm: any;
-  // @Output() isBroadcastStreamChange = new EventEmitter<boolean>();
+
   liveStreamObj: any = [];
   broadcastCategories: any;
   constants: any = CONSTANTS;
   newBroadCastForm: any;
   videoDevices: any;
   audioDevices: any;
+  client: any;
+  config: any;
   isLoading: boolean = false;
   broadcastClient = (window as any).IVSBroadcastClient;
   constructor(
@@ -29,16 +32,18 @@ export class BroadcastStreamComponent implements OnInit {
     @Inject(DOCUMENT) private document: any,
     public _globalFunctions: GlobalFunctions,
     private _activatedRoute: ActivatedRoute,
+    private elementRef: ElementRef,
     private _router: Router,
     private _liveStreamService: LiveStreamService,
   ) {
     this._prepareBroadCastLiveStreamForm();
+    this.getLiveStreamObj();
   }
 
   async ngOnInit() {
     this.videoDevices = await this.getVideoDevices();
     this.audioDevices = await this.getAudioDevices();
-    this.getLiveStreamObj();
+    //this.handlePermissions();
   }
   _prepareBroadCastLiveStreamForm() {
     this.newBroadCastForm = this._formBuilder.group({
@@ -47,11 +52,28 @@ export class BroadcastStreamComponent implements OnInit {
       channel_config: ['', Validators.required],
     });
   }
-  getLiveStreamObj(): void {
+   getLiveStreamObj(): void {
     this.isLoading = true;
     const liveStreamId = this._activatedRoute.snapshot.paramMap.get('id');
     this._liveStreamService.getLiveStreamById(liveStreamId).subscribe((result: any) => {
       this.liveStreamObj = result.Data;
+      this.config = {
+        ingestEndpoint: 'rtmps://'+this.liveStreamObj.awsivschannelconfig.channel.ingestEndpoint+':443/app/',
+        streamConfig: this.broadcastClient.BASIC_LANDSCAPE,
+        logLevel: this.broadcastClient.LOG_LEVEL.DEBUG,
+        streamKey: this.liveStreamObj.awsivschannelconfig.streamKey.value
+      };
+
+      this.client = this.broadcastClient.create({
+        ingestEndpoint: this.config.ingestEndpoint,
+        streamConfig: (window as any).IVSBroadcastClient.BASIC_LANDSCAPE
+      });
+      const previewEl = this.document.getElementById("preview");
+      console.log(previewEl);
+      console.log(this.client);
+      this.client.attachPreview(previewEl);
+       this.handleVideoDeviceSelect(event);
+       this.handleAudioDeviceSelect(event);
       this.isLoading = false;
     }, (error: any) => {
       this._globalFunctions.errorHanding(error, this, true);
@@ -69,12 +91,8 @@ export class BroadcastStreamComponent implements OnInit {
       return;
     } else {
       console.log(this.broadcastClient);
-      const client = this.broadcastClient.create({
-        streamConfig: this.broadcastClient.BASIC_LANDSCAPE,
-        ingestEndpoint: 'rtmps://76a988f0fedf.global-contribute.live-video.net:443/app/',
-      });
-      let streamKey = 'sk_ap-south-1_JwHGxlTjDqRt_lPH5lL3Y2umbCVpEl7nLeoOTBwZkVM';
-      client.startBroadcast(streamKey).then((result: any) => {
+      let streamKey = this.config.streamKey;
+      this.client.startBroadcast(streamKey).then((result: any) => {
         console.log('I am successfully broadcasting!');
       })
         .catch((error: any) => {
@@ -84,7 +102,64 @@ export class BroadcastStreamComponent implements OnInit {
     }
   }
   stopBroadcast() {
+     this.client.stopBroadcast();
+  }
+  async handleVideoDeviceSelect(event: any){
+    console.log(event.target.value);
+    const id = "camera";
+    const  devices = await this.getVideoDevices();
+    if (this.client.getVideoInputDevice(id)) {
+      this.client.removeVideoInputDevice(id);
+    }
 
+    // Get the option's video
+    const selectedDevice = devices.find(
+        (device: any) => device.deviceId === event.target.value
+    );
+    console.log(selectedDevice);
+    const deviceId = selectedDevice ? selectedDevice.deviceId : null;
+    const { width, height } = this.config.streamConfig.maxResolution;
+    const cameraStream = await this.getCamera(deviceId, width, height);
+    console.log(this.client);
+    // Add the camera to the top
+    await this.client.addVideoInputDevice(cameraStream, id, {
+      index: 0
+    });
+  }
+
+  handleAudioDeviceSelect(event: any){
+    console.log(event.target.value);
+  }
+
+  async getCamera(deviceId: any, maxWidth: any, maxHeight: any) {
+    let media;
+    let videoConstraints: any = {
+      deviceId: deviceId ? { exact: deviceId } : null,
+      width: {
+        max: maxWidth
+      },
+      height: {
+        max: maxHeight
+      }
+    };
+    try {
+      // Let's try with max width and height constraints
+      media = await navigator.mediaDevices.getUserMedia({
+        video: videoConstraints,
+        audio: true
+      });
+    } catch (e) {
+      console.log('error'+e);
+      this.handlePermissions();
+      // and fallback to unconstrained result
+      delete videoConstraints.width;
+      delete videoConstraints.height;
+      media = await navigator.mediaDevices.getUserMedia({
+        video: videoConstraints
+      });
+    }
+    console.log(media);
+    return media;
   }
   prepareEventObj(eventObj: any = {}, isForUpdateEvent: boolean = false): any {
     const preparedEventObj: any = this._globalFunctions.copyObject(eventObj);
@@ -93,22 +168,42 @@ export class BroadcastStreamComponent implements OnInit {
 
   async getVideoDevices() {
     const devices = await navigator.mediaDevices.enumerateDevices();
-    const videoDevices = devices.filter((d) => d.kind === "videoinput");
+    const videoDevices = devices.filter((d: any) => d.kind === "videoinput");
     if (!videoDevices.length) {
-
+      console.log("No video devices found.");
     }
-    console.log(videoDevices);
     return videoDevices;
   }
 
   async getAudioDevices() {
     const devices = await navigator.mediaDevices.enumerateDevices();
-    const audioDevices = devices.filter((d) => d.kind === "audioinput");
+    const audioDevices = devices.filter((d: any) => d.kind === "audioinput");
     if (!audioDevices.length) {
       console.log("No audio devices found.");
     }
-    console.log(audioDevices);
     return audioDevices;
+  }
+
+  async handlePermissions() {
+    try {
+      const permissionsStatus = await navigator.permissions.query({ name: 'camera' });
+      console.log(permissionsStatus)
+      if (permissionsStatus.state === 'granted') {
+        console.log('Camera permission already granted.');
+      } else if (permissionsStatus.state === 'prompt') {
+        const newPermissionsStatus = await navigator.permissions.request({ name: 'camera' });
+        if (newPermissionsStatus.state === 'granted') {
+          console.log('Camera permission granted.');
+        } else {
+          console.log('Camera permission denied.');
+        }
+      } else {
+        console.log('Camera permission denied.');
+      }
+    } catch (error) {
+      console.error('Error requesting camera permission:', error);
+    }
+
   }
 
 }
