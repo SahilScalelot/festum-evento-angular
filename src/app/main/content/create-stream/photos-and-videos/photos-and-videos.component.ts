03f7de4bab036782f3da7d00cad235247b8f1287
lalji <lalji.scalelot@gmail.com>
2023-09-05 17:47:32 +0530

    update livestream with image & video modal edit delete

--- src/app/main/content/create-stream/photos-and-videos/photos-and-videos.component.ts
+++ src/app/main/content/create-stream/photos-and-videos/photos-and-videos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { ImageCroppedEvent } from 'ngx-image-cropper';
 import { Router } from '@angular/router';
@@ -18,18 +18,29 @@ declare var $: any;
   styleUrls: ['./photos-and-videos.component.scss']
 })
 export class PhotosAndVideosComponent implements OnInit {
+  @ViewChild('photosNgForm') photosNgForm: any;
+  @ViewChild('videosNgForm') videosNgForm: any;
+  @ViewChild('photoEditForm') photoEditForm: any;
+
   constants: any = CONSTANTS;
   liveStreamId: any = '';
   deleteItemObj: any = {};
   
   imgChangeEvt: any = '';
   cropImgPreview: any = '';
+  photoForm: any;
+  videoForm: any;
+  photoUpdateForm: any;
   posterObj: any = {};
   dropifyOption: any = {};
+  dropifyEditOption: any = {};
+  editPhotoObj: any = {};
   isCropperLoading: boolean = false;
   isPosterLoading: boolean = false;
+  isPhotoLoading: boolean = false;
   isEditMode: boolean = false;
   drEvent: any;
+  editDrEvent: any;
   bannerUrl: any = '';
   
   photoArr: any = [];  
@@ -46,6 +57,7 @@ export class PhotosAndVideosComponent implements OnInit {
   videosUploadLimit: number = 2;
   rejectedVideosList: any;
   videosFiles: File[] = [];
+  descriptionLimit: any = 0;
 
   constructor(
     private _formBuilder: FormBuilder,
@@ -58,13 +70,44 @@ export class PhotosAndVideosComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.dropifyOption = {
+      messages: {
+        default: 'Add Poster',
+        icon: '<svg width="21" height="17" viewBox="0 0 21 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19.6666 0.333496H1.33335C0.59702 0.333496 0 0.930479 0 1.66681V15.3335C0 16.0698 0.59702 16.6668 1.33335 16.6668H19.6666C20.403 16.6668 21 16.0698 21 15.3335V1.66681C21 0.930479 20.403 0.333496 19.6666 0.333496ZM19.6666 1.66681V11.3638L17.0389 8.9748C16.644 8.61581 16.0366 8.63014 15.6593 9.00782L12.9999 11.6668L7.75634 5.40347C7.35998 4.93013 6.63397 4.92548 6.23167 5.39314L1.33335 11.0858V1.66681H19.6666ZM14 5.16682C14 4.15414 14.8206 3.33347 15.8333 3.33347C16.846 3.33347 17.6666 4.15414 17.6666 5.16682C17.6666 6.17949 16.846 7.00012 15.8333 7.00012C14.8206 7.00016 14 6.17949 14 5.16682Z" fill="#A6A6A6"/></svg>',
+      }
+    };
+    this.dropifyEditOption = {
+      messages: {
+        default: 'Add Image',
+        icon: '<svg width="21" height="17" viewBox="0 0 21 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19.6666 0.333496H1.33335C0.59702 0.333496 0 0.930479 0 1.66681V15.3335C0 16.0698 0.59702 16.6668 1.33335 16.6668H19.6666C20.403 16.6668 21 16.0698 21 15.3335V1.66681C21 0.930479 20.403 0.333496 19.6666 0.333496ZM19.6666 1.66681V11.3638L17.0389 8.9748C16.644 8.61581 16.0366 8.63014 15.6593 9.00782L12.9999 11.6668L7.75634 5.40347C7.35998 4.93013 6.63397 4.92548 6.23167 5.39314L1.33335 11.0858V1.66681H19.6666ZM14 5.16682C14 4.15414 14.8206 3.33347 15.8333 3.33347C16.846 3.33347 17.6666 4.15414 17.6666 5.16682C17.6666 6.17949 16.846 7.00012 15.8333 7.00012C14.8206 7.00016 14 6.17949 14 5.16682Z" fill="#A6A6A6"/></svg>',
+      }
+    };
     this.drEvent = $('#poster').dropify(this.dropifyOption);
-    
+    this.editDrEvent = $('.editPoster').dropify(this.dropifyEditOption);
     this.liveStreamId = localStorage.getItem('lsId');
     if (this.liveStreamId && this.liveStreamId != '') {
       this.isEditMode = true;
       this.getLiveStreamMediaById(this.liveStreamId);
     }
+
+    this.photoForm = this._formBuilder.group({
+      image: [null],
+      imageName: [''],
+      description: [null]
+    });
+
+    this.photoUpdateForm = this._formBuilder.group({
+      image: [null],
+      imageName: [''],
+      description: [null]
+    });
+
+
+    this.videoForm = this._formBuilder.group({
+      video: [null],
+      videoName: [null],
+      description: [null]
+    });
   }
 
   getLiveStreamMediaById(liveStreamId: any = ''): void {
@@ -153,6 +196,26 @@ export class PhotosAndVideosComponent implements OnInit {
     }
   }
 
+  openUploadPhotoDialog(): void {
+    this.descriptionLimit = 0;
+    this.photosNgForm.resetForm();
+    if (this.photoArr && this.photoArr.length && this.photoArr.length >= this.photosUploadLimit) {
+      this._sNotify.error('Maximum 15 images can upload!', 'Oops!');
+    } else {
+      this._modalService.open('photo');
+    }
+  }
+
+  openUploadVideoDialog(): void {
+    this.descriptionLimit = 0;
+    this.videosNgForm.resetForm();
+    if (this.videoArr && this.videoArr.length && this.videoArr.length >= 2) {
+      this._sNotify.error('Maximum 2 videos can upload!', 'Oops!');
+    } else {
+      this._modalService.open('video');
+    }
+  }
+
   setPosterInDropify(image: any = ''): void {
     const imageUrl = CONSTANTS.baseImageURL + image;
     this.drEvent = this.drEvent.data('dropify');
@@ -186,7 +249,13 @@ export class PhotosAndVideosComponent implements OnInit {
 
   // Upload Images  
   uploadImage(): any {
+    if (this.descriptionLimit > CONSTANTS.CKEditorCharacterLimit0) {
+      return false;
+    }
+
     const responseObj: Observable<any>[] = [];
+    console.log(responseObj);
+
     this.imagesFiles.forEach((image: any) => {
       if (image != undefined) {
         if (image.type != 'image/jpeg' && image.type != 'image/jpg' && image.type != 'image/png') {
@@ -208,32 +277,32 @@ export class PhotosAndVideosComponent implements OnInit {
 
         const photoFormData = new FormData();
         photoFormData.append('file', image);
-        this.isImgLoading = true;
+        this.isPhotoLoading = true;
         responseObj.push(this._createStreamService.uploadImages(photoFormData));
       }
     });
 
-    forkJoin(...responseObj)
-    .pipe(
-      switchMap((result: any) => {
-        return result;
-      })
-    )
-    .subscribe((result: any) => {
-      if (result && result.IsSuccess) {
-        this.photoArr.push({ url: result.Data.url });
-        this._sNotify.success('Image Uploaded Successfully.', 'Success');
-        this.imagesFiles = [];
-        this.isImgLoading = false;
-      } else {
-        this._globalFunctions.successErrorHandling(result, this, true);
-        this.isImgLoading = false;
-      }
+    forkJoin(...responseObj).subscribe((resultArr: any) => {
+      _.each(resultArr, (result: any) => {
+          if (result && result.IsSuccess) {
+            this.photoArr.push({ url: result.Data.url, description: this.photoForm.value?.description });
+            this.photoForm.get('description').setValue('');
+            this._sNotify.success('Image Uploaded Successfully.', 'Success');
+            this.imagesFiles = [];
+            this.isPhotoLoading = false;
+            this.descriptionLimit = 0;
+            this._modalService.close('photo');
+          } else {
+            this._globalFunctions.successErrorHandling(result, this, true);
+            this.isImgLoading = false;
+          }
+      });
     }, (error: any) => {
       this._globalFunctions.errorHanding(error, this, true);
-      this.isImgLoading = false;
+      this.isPhotoLoading = false;
     });
   }
+
   onSelectImages(event: any) {
     const totalPhotos = this.photosUploadLimit - ((this.photoArr?.length || 0) + (event?.addedFiles?.length || 0) + (this.imagesFiles?.length || 0));
     if ((totalPhotos >= 0) && (totalPhotos <= this.photosUploadLimit)) {
@@ -243,11 +312,88 @@ export class PhotosAndVideosComponent implements OnInit {
       this._sNotify.warning('You have exceeded the maximum photos limit!', 'Oops..');
     }
   }
+
   onRemoveImages(event: any) {
     this.imagesFiles.splice(this.imagesFiles.indexOf(event), 1);
   }
-  editImage(event: any, index:number) {
 
+  editImage(photo: any, index:number) {
+    console.log(this.photoArr);
+    console.log(this.photoArr[index]);
+    this.editPhotoObj = { index: index, type: 'photo', data: this.photoArr[index]};
+    //console.log(photo);
+    this.photoUpdateForm.controls['imageName'].setValue(this.constants.baseImageURL + photo.url);
+    this.photoUpdateForm.controls['description'].setValue(photo.description);
+    this.editDrEvent = this.editDrEvent.data('dropify');
+    this.editDrEvent.resetPreview();
+    this.editDrEvent.clearElement();
+    this.editDrEvent.settings.defaultFile = this.constants.baseImageURL + photo.url;
+    this.editDrEvent.destroy();
+    this.editDrEvent.init();
+    this.dropifyEditOption.defaultFile = this.constants.baseImageURL + photo.url;
+    this.editDrEvent = $('.editPoster').dropify(this.dropifyEditOption);
+    this._modalService.open("photoEdit");
+  }
+
+  onEditPosterChange(event: any): any {
+    this.imgChangeEvt = event;
+    if (event.target.files.length > 0) {
+      const poster = event.target.files[0];
+      if (poster != undefined) {
+        if (poster.type != 'image/jpeg' && poster.type != 'image/jpg' && poster.type != 'image/png' && poster.type != 'image/gif' && poster.type != 'image/avif' && poster.type != 'image/raw') {
+          this._sNotify.error('Images type should only jpeg, jpg, png, gif, avif and raw.', 'Oops!');
+          return false;
+        }
+
+        const image_size = poster.size / 1024 / 1024;
+        if (image_size > CONSTANTS.maxPosterSizeInMB) {
+          this._sNotify.error('Maximum Poster Size is ' + CONSTANTS.maxPosterSizeInMB + 'MB.', 'Oops!');
+          return false;
+        }
+        this.photoUpdateForm.controls['image'].setValue(poster);
+        //this.savePoster(poster);
+      }
+    }
+  }
+  // Images Upload for Edit & Check image exist & new
+  editUploadImage(): any {
+    //console.log(this.editPhotoObj);
+    //console.log(this.photoUpdateForm.value);
+    if (this.photoUpdateForm.value.image === null) {
+      //console.log('Empty New File Attachment');
+      this.photoArr[this.editPhotoObj.index].description = this.photoUpdateForm.value.description;
+      this._modalService.close('photoEdit');
+      this.savePhotosVideo();
+    } else {
+      if (this.photoUpdateForm.value.image !== null) {
+        //console.log('Ready For Upload New File');
+        const photoFormData = new FormData();
+        photoFormData.append('file', this.photoUpdateForm.value.image);
+        this.isPhotoLoading = true;
+        this._createStreamService.uploadImages(photoFormData).subscribe((result: any) => {
+          if (result && result.IsSuccess) {
+            //console.log(result);
+            this.photoArr[this.editPhotoObj.index].url = result.Data.url;
+            this.photoArr[this.editPhotoObj.index].description = this.photoUpdateForm.value.description;
+            //this.posterObj.image = compressedImage;
+            //this.setPosterInDropify(result.Data.url);
+            // this.inputText = _.last(_.split(result.Data.url, '/'));
+            this._sNotify.success('File Uploaded Successfully.', 'Success');
+            this.isPhotoLoading = false;
+            this._modalService.close("photoEdit");
+            this.savePhotosVideo()
+          } else {
+            this._globalFunctions.successErrorHandling(result, this, true);
+            this.isPosterLoading = false;
+          }
+        }, (error: any) => {
+          this._globalFunctions.errorHanding(error, this, true);
+          this.isPosterLoading = false;
+        });
+      } else {
+        this._sNotify.success('Something went wrong!', 'Oops');
+      }
+    }
   }
   removeImage(index: number) {
     this.deleteItemObj = {index: index, type: 'photo'};
@@ -285,11 +431,13 @@ export class PhotosAndVideosComponent implements OnInit {
     forkJoin(...responseObj).subscribe((resultArr: any) => {
       _.each(resultArr, (result: any) => {
         if (result && result.IsSuccess) {
-          this.videoArr.push({ url: result.Data.url });
+          this.videoArr.push({ url: result.Data.url, description: this.videoForm.value?.description });
+          this.videoForm.get('description').setValue('');
           this._sNotify.success('Video Uploaded Successfully.', 'Success');
           this.videosFiles = [];
           this.isVideoLoading = false;
           $('#create-video-upload').val(null);
+          this._modalService.close('video');
         } else {
           this._globalFunctions.successErrorHandling(result, this, true);
           this.isVideoLoading = false;
@@ -333,7 +481,24 @@ export class PhotosAndVideosComponent implements OnInit {
         break;
     }
     this.isDeleteLoading = false;
+    this.savePhotosVideo();
     this.close();
+  }
+
+  savePhotosVideo() {
+    const preparedPhotosAndVideosObj: any = this.preparePhotosAndVideosObj();
+    this._createStreamService.saveLiveStreamMedia(preparedPhotosAndVideosObj).subscribe((result: any) => {
+      if (result && result.IsSuccess) {
+        this.isLoading = false;
+        this._sNotify.success('Image Updated Successfully.', 'Success');
+      } else {
+        this._globalFunctions.successErrorHandling(result, this, true);
+        this.isLoading = false;
+      }
+    }, (error: any) => {
+      this._globalFunctions.errorHanding(error, this, true);
+      this.isLoading = false;
+    });
   }
 
   validatePhotosAndVideosObj(): any {
