7f6fe0ceb7f979a649fc41c9ee9afbb6b06384de
lalji <lalji.scalelot@gmail.com>
2023-09-06 14:19:46 +0530

    add & update livestream photo & videos step operation

--- src/app/main/content/create-stream/photos-and-videos/photos-and-videos.component.ts
+++ src/app/main/content/create-stream/photos-and-videos/photos-and-videos.component.ts
@@ -21,6 +21,7 @@ export class PhotosAndVideosComponent implements OnInit {
   @ViewChild('photosNgForm') photosNgForm: any;
   @ViewChild('videosNgForm') videosNgForm: any;
   @ViewChild('photoEditForm') photoEditForm: any;
+  @ViewChild('videoEditForm') videoEditForm: any;
 
   constants: any = CONSTANTS;
   liveStreamId: any = '';
@@ -31,20 +32,30 @@ export class PhotosAndVideosComponent implements OnInit {
   photoForm: any;
   videoForm: any;
   photoUpdateForm: any;
+  videoUpdateForm: any;
   posterObj: any = {};
   dropifyOption: any = {};
   dropifyEditOption: any = {};
+  dropifyVideoEditOption: any = {};
   editPhotoObj: any = {};
+  editVideoObj: any = {};
   isCropperLoading: boolean = false;
   isPosterLoading: boolean = false;
   isPhotoLoading: boolean = false;
   isEditMode: boolean = false;
   drEvent: any;
   editDrEvent: any;
+  editVideoDrEvent: any;
   bannerUrl: any = '';
   
   photoArr: any = [];  
   videoArr: any = [];
+
+  isOpenPopup: boolean = false;
+  isSingleVideo: boolean = false;
+  companyIAndV: boolean = false;
+  isImage: boolean = false;
+  imagesOrVideosArr: Array<any> = [];
 
   isLoading: boolean = false;
   isImgLoading: boolean = false;
@@ -82,8 +93,15 @@ export class PhotosAndVideosComponent implements OnInit {
         icon: '<svg width="21" height="17" viewBox="0 0 21 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19.6666 0.333496H1.33335C0.59702 0.333496 0 0.930479 0 1.66681V15.3335C0 16.0698 0.59702 16.6668 1.33335 16.6668H19.6666C20.403 16.6668 21 16.0698 21 15.3335V1.66681C21 0.930479 20.403 0.333496 19.6666 0.333496ZM19.6666 1.66681V11.3638L17.0389 8.9748C16.644 8.61581 16.0366 8.63014 15.6593 9.00782L12.9999 11.6668L7.75634 5.40347C7.35998 4.93013 6.63397 4.92548 6.23167 5.39314L1.33335 11.0858V1.66681H19.6666ZM14 5.16682C14 4.15414 14.8206 3.33347 15.8333 3.33347C16.846 3.33347 17.6666 4.15414 17.6666 5.16682C17.6666 6.17949 16.846 7.00012 15.8333 7.00012C14.8206 7.00016 14 6.17949 14 5.16682Z" fill="#A6A6A6"/></svg>',
       }
     };
+    this.dropifyVideoEditOption = {
+      messages: {
+        default: 'Add Video',
+        icon: '<svg width="21" height="17" viewBox="0 0 21 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19.6666 0.333496H1.33335C0.59702 0.333496 0 0.930479 0 1.66681V15.3335C0 16.0698 0.59702 16.6668 1.33335 16.6668H19.6666C20.403 16.6668 21 16.0698 21 15.3335V1.66681C21 0.930479 20.403 0.333496 19.6666 0.333496ZM19.6666 1.66681V11.3638L17.0389 8.9748C16.644 8.61581 16.0366 8.63014 15.6593 9.00782L12.9999 11.6668L7.75634 5.40347C7.35998 4.93013 6.63397 4.92548 6.23167 5.39314L1.33335 11.0858V1.66681H19.6666ZM14 5.16682C14 4.15414 14.8206 3.33347 15.8333 3.33347C16.846 3.33347 17.6666 4.15414 17.6666 5.16682C17.6666 6.17949 16.846 7.00012 15.8333 7.00012C14.8206 7.00016 14 6.17949 14 5.16682Z" fill="#A6A6A6"/></svg>',
+      }
+    };
     this.drEvent = $('#poster').dropify(this.dropifyOption);
     this.editDrEvent = $('.editPoster').dropify(this.dropifyEditOption);
+    this.editVideoDrEvent = $('.editVideo').dropify(this.dropifyVideoEditOption);
     this.liveStreamId = localStorage.getItem('lsId');
     if (this.liveStreamId && this.liveStreamId != '') {
       this.isEditMode = true;
@@ -104,6 +122,12 @@ export class PhotosAndVideosComponent implements OnInit {
 
 
     this.videoForm = this._formBuilder.group({
+      video: [null],
+      videoName: [null],
+      description: [null]
+    });
+
+    this.videoUpdateForm = this._formBuilder.group({
       video: [null],
       videoName: [null],
       description: [null]
@@ -254,7 +278,7 @@ export class PhotosAndVideosComponent implements OnInit {
     }
 
     const responseObj: Observable<any>[] = [];
-    console.log(responseObj);
+
 
     this.imagesFiles.forEach((image: any) => {
       if (image != undefined) {
@@ -318,10 +342,7 @@ export class PhotosAndVideosComponent implements OnInit {
   }
 
   editImage(photo: any, index:number) {
-    console.log(this.photoArr);
-    console.log(this.photoArr[index]);
     this.editPhotoObj = { index: index, type: 'photo', data: this.photoArr[index]};
-    //console.log(photo);
     this.photoUpdateForm.controls['imageName'].setValue(this.constants.baseImageURL + photo.url);
     this.photoUpdateForm.controls['description'].setValue(photo.description);
     this.editDrEvent = this.editDrEvent.data('dropify');
@@ -357,27 +378,19 @@ export class PhotosAndVideosComponent implements OnInit {
   }
   // Images Upload for Edit & Check image exist & new
   editUploadImage(): any {
-    //console.log(this.editPhotoObj);
-    //console.log(this.photoUpdateForm.value);
     if (this.photoUpdateForm.value.image === null) {
-      //console.log('Empty New File Attachment');
       this.photoArr[this.editPhotoObj.index].description = this.photoUpdateForm.value.description;
       this._modalService.close('photoEdit');
       this.savePhotosVideo();
     } else {
       if (this.photoUpdateForm.value.image !== null) {
-        //console.log('Ready For Upload New File');
         const photoFormData = new FormData();
         photoFormData.append('file', this.photoUpdateForm.value.image);
         this.isPhotoLoading = true;
         this._createStreamService.uploadImages(photoFormData).subscribe((result: any) => {
           if (result && result.IsSuccess) {
-            //console.log(result);
             this.photoArr[this.editPhotoObj.index].url = result.Data.url;
             this.photoArr[this.editPhotoObj.index].description = this.photoUpdateForm.value.description;
-            //this.posterObj.image = compressedImage;
-            //this.setPosterInDropify(result.Data.url);
-            // this.inputText = _.last(_.split(result.Data.url, '/'));
             this._sNotify.success('File Uploaded Successfully.', 'Success');
             this.isPhotoLoading = false;
             this._modalService.close("photoEdit");
@@ -460,6 +473,31 @@ export class PhotosAndVideosComponent implements OnInit {
   onRemoveVideos(event: any) {
     this.videosFiles.splice(this.videosFiles.indexOf(event), 1);
   }
+
+  editVideoModal(video: any, index:number) {
+    this.editVideoObj = { index: index, type: 'video', data: this.videoArr[index]};
+    this.videoUpdateForm.controls['videoName'].setValue(this.constants.baseImageURL + video.url);
+    this.videoUpdateForm.controls['description'].setValue(video.description);
+    this.editVideoDrEvent = this.editVideoDrEvent.data('dropify');
+    this.editVideoDrEvent.resetPreview();
+    this.editVideoDrEvent.clearElement();
+    this.editVideoDrEvent.settings.defaultFile = this.constants.baseImageURL + video.url;
+    this.editVideoDrEvent.destroy();
+    this.editVideoDrEvent.init();
+    this.dropifyVideoEditOption.defaultFile = this.constants.baseImageURL + video.url;
+    this.editVideoDrEvent = $('.editVideo').dropify(this.dropifyVideoEditOption);
+    this._modalService.open("videoEdit");
+  }
+  openImageAndVideoDialog(imagesOrVideosArr: Array<any>, isImage: boolean, companyIAndV: boolean,  isSingleVideo: boolean = false): void {
+    this.imagesOrVideosArr = imagesOrVideosArr;
+    this.isImage = isImage;
+    this.companyIAndV = companyIAndV;
+    this.isSingleVideo = isSingleVideo;
+    this.isOpenPopup = true;
+  }
+  closePop(flag: boolean): void {
+    this.isOpenPopup = flag;
+  }
   removeVideo(index: number) {
     this.deleteItemObj = {index: index, type: 'video'};
     this._modalService.open("delete-event-pop");
@@ -483,6 +521,58 @@ export class PhotosAndVideosComponent implements OnInit {
     this.isDeleteLoading = false;
     this.savePhotosVideo();
     this.close();
+  }
+
+  onEditVideoChange(event: any): any {
+    this.imgChangeEvt = event;
+    if (event.target.files.length > 0) {
+      const video = event.target.files[0];
+      if (video != undefined) {
+        if (video.type != 'video/mp4' && video.type != 'video/x-m4v' && video.type != 'video/*') {
+          this._sNotify.error('Videos type should only mp4, avi and raw.', 'Oops!');
+          return false;
+        }
+
+        const image_size = video.size / 1024 / 1024;
+        if (image_size > CONSTANTS.maxVideoSizeInMB) {
+          this._sNotify.error('Maximum Video Size is ' + CONSTANTS.maxVideoSizeInMB + 'MB.', 'Oops!');
+          return false;
+        }
+        this.videoUpdateForm.controls['video'].setValue(video);
+      }
+    }
+  }
+
+  editUploadVideo(): any {
+    if (this.videoUpdateForm.value.video === null) {
+      this.videoArr[this.editVideoObj.index].description = this.videoUpdateForm.value.description;
+      this._modalService.close('photoEdit');
+      this.savePhotosVideo();
+    } else {
+      if (this.videoUpdateForm.value.video !== null) {
+        const videoFormData = new FormData();
+        videoFormData.append('file', this.videoUpdateForm.value.video);
+        this.isVideoLoading = true;
+        this._createStreamService.uploadVideos(videoFormData).subscribe((result: any) => {
+          if (result && result.IsSuccess) {
+            this.videoArr[this.editVideoObj.index].url = result.Data.url;
+            this.videoArr[this.editVideoObj.index].description = this.videoUpdateForm.value.description;
+            this._sNotify.success('File Uploaded Successfully.', 'Success');
+            this.isVideoLoading = false;
+            this._modalService.close("videoEdit");
+            this.savePhotosVideo()
+          } else {
+            this._globalFunctions.successErrorHandling(result, this, true);
+            this.isPosterLoading = false;
+          }
+        }, (error: any) => {
+          this._globalFunctions.errorHanding(error, this, true);
+          this.isPosterLoading = false;
+        });
+      } else {
+        this._sNotify.success('Something went wrong!', 'Oops');
+      }
+    }
   }
 
   savePhotosVideo() {
