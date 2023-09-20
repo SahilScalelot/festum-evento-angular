b1fae3bf7e8a1f1c2f0785a5015165091135532b
lalji <lalji.scalelot@gmail.com>
2023-09-20 15:36:28 +0530

    update socket io service with receive message

--- src/app/services/socketio.service.ts
+++ src/app/services/socketio.service.ts
@@ -1,15 +1,32 @@
 import { environment } from 'src/environments/environment';
 import { Injectable } from '@angular/core';
 import { io } from 'socket.io-client';
+import { Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
 })
 
 export class SocketioService {
+    private socket: any;
 
-    socket: any;
-    constructor() {   }
+    constructor() {
+        this.socket = io(environment.SOCKET_ENDPOINT)
+    }
+
+    // Emit an event
+    public sendMessage(message: string): void {
+        this.socket.emit('notification', { message });
+    }
+
+    // Receive notifications
+    public receiveMessage(): Observable<any> {
+        return new Observable<any>((observer) => {
+            this.socket.on('notification', (data: any) => {
+                observer.next(data);
+            });
+        });
+    }
 
     setupSocketConnection() {
         this.socket = io(environment.SOCKET_ENDPOINT, {
