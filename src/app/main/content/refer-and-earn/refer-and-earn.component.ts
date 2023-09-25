e0153255f49794221d90641b68af87666a3dda6b
lalji <lalji.scalelot@gmail.com>
2023-09-25 13:51:07 +0530

    update share option url in refer & earn

--- src/app/main/content/refer-and-earn/refer-and-earn.component.ts
+++ src/app/main/content/refer-and-earn/refer-and-earn.component.ts
@@ -12,6 +12,8 @@ import { Clipboard } from '@angular/cdk/clipboard';
 export class ReferAndEarnComponent implements OnInit {
   isLoading: boolean = false;
   userObj: any = {};
+  referralCode: string = '';
+  referralLink: string = `https://festumevento.com/?ref=${this.referralCode}`;
 
   constructor(
     private _globalService: GlobalService,
@@ -25,9 +27,46 @@ export class ReferAndEarnComponent implements OnInit {
       this.isLoading = true;
       if (user) {
         this.userObj = this._globalFunctions.copyObject(user);
+        this.referralCode = this.userObj.my_refer_code;
         this.isLoading = false;
       }
     });
+  }
+
+  shareOnWhatsApp() {
+    const text = `Check out this amazing offer! Use my referral code: ${this.referralCode}`;
+    const url = `https://api.whatsapp.com/send?text=${encodeURIComponent(text)}`;
+    window.open(url, '_blank');
+  }
+
+  shareOnFacebook() {
+    const shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(this.referralLink)}`;
+    window.open(shareUrl, '_blank');
+  }
+
+  shareOnTelegram() {
+    const text = `Check out this amazing offer! Use my referral code: ${this.referralCode}`;
+    const url = `https://t.me/share/url?url=${encodeURIComponent(this.referralLink)}&text=${encodeURIComponent(text)}`;
+    window.open(url, '_blank');
+  }
+
+  shareOnTwitter() {
+    const text = `Check out this amazing offer! Use my referral code: ${this.referralCode}`;
+    const shareUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(this.referralLink)}&text=${encodeURIComponent(text)}`;
+    window.open(shareUrl, '_blank');
+  }
+
+  shareViaSMS() {
+    const message = `Check out this amazing offer! Use my referral code: ${this.referralCode}`;
+    const url = `sms:?&body=${encodeURIComponent(message)}`;
+    window.location.href = url;
+  }
+
+  shareByEmail() {
+    const subject = 'Check out this amazing offer!';
+    const body = `Use my referral code: ${this.referralCode}\n\n${this.referralLink}`;
+    const url = `mailto:?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+    window.location.href = url;
   }
 
   copyLink(copyText: any) {
