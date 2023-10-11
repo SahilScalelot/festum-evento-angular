acb72544c724d0a9122ee5e5f272825a04a6e88a
lalji <lalji.scalelot@gmail.com>
2023-10-11 13:49:49 +0530

    added translation message in faq & help

--- src/app/main/content/help/help.module.ts
+++ src/app/main/content/help/help.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from "@ngx-translate/core";
 import { HelpComponent } from './help.component';
 import { HelpService } from './help.service';
 
 @NgModule({
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, TranslateModule],
   declarations: [HelpComponent],
   providers: [HelpService],
 })
