a4a2104445ff80ea5cf41b047da145f2da44383c
lalji <lalji.scalelot@gmail.com>
2023-10-23 18:13:38 +0530

    fix broken profile pic & last message time format

--- src/app/main/content/event/event-chat/event-chat.component.ts
+++ src/app/main/content/event/event-chat/event-chat.component.ts
@@ -62,8 +62,38 @@ export class EventChatComponent implements OnInit {
     });
   }
 
-  formatDate(timestamp: number): string {
-    return this.datePipe.transform(timestamp, 'yyyy-MM-dd HH:mm:ss');
+  formatDate(timestamp: number): any {
+    if (timestamp) {
+      const currentDate = new Date();
+      const targetDate = new Date(timestamp);
+
+      const timeDifference = currentDate.getTime() - targetDate.getTime();
+
+      const years = Math.floor(timeDifference / (1000 * 60 * 60 * 24 * 365));
+      const months = Math.floor(timeDifference / (1000 * 60 * 60 * 24 * 30));
+      const weeks = Math.floor(timeDifference / (1000 * 60 * 60 * 24 * 7));
+      const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(timeDifference / (1000 * 60 * 60));
+      const minutes = Math.floor(timeDifference / (1000 * 60));
+      const seconds = Math.floor(timeDifference / 1000);
+
+      if (years > 0) {
+        return `${years} year${years > 1 ? 's' : ''} ago`;
+      } else if (months > 0) {
+        return `${months} month${months > 1 ? 's' : ''} ago`;
+      } else if (weeks > 0) {
+        return `${weeks} week${weeks > 1 ? 's' : ''} ago`;
+      } else if (days > 0) {
+        return `${days} day${days > 1 ? 's' : ''} ago`;
+      } else if (hours > 0) {
+        return `${hours} hour${hours > 1 ? 's' : ''} ago`;
+      } else if (minutes > 0) {
+        return `${minutes} minute${minutes > 1 ? 's' : ''} ago`;
+      } else {
+        return `${seconds} second${seconds > 1 ? 's' : ''} ago`;
+      }
+    }
+    //return this.datePipe.transform(timestamp, 'yyyy-MM-dd HH:mm:ss');
   }
 
   selectUser(index: number, user: any): void {
